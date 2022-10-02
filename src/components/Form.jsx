2b604f2618024ebc7c0b03d56b1510f36e397f62
Anton Semenov <anseme@gmail.com>
2022-10-02 14:59:19 +0200

    Add more fields and styles to the form

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -8,9 +8,39 @@ function Form() {
         Add Patients for {''}
         <span className="text-indigo-600 font-bold">Manage</span>
       </p>
-      <form className="bg-white shadow-md mb-10 py-10 px-5">
-        <label className="text-gray-700 uppercase">Name</label>
-        <input type="text" className="p-2 w-full border placeholder-red-300" placeholder="Enter the name here" />
+      <form className="bg-white shadow-md mb-10 py-10 px-5 rounded-md">
+        <div className="mb-5">
+        <label htmlFor="pet_name" className="text-gray-700 uppercase">Pet Name</label>
+        <input id="pet_name" type="text" className="p-2 w-full border placeholder-gray-400 rounded-md" placeholder="Enter the Pet Name here" />
+        </div>
+
+        <div className="mb-5">   
+        <label htmlFor="owner_name" className="text-gray-700 uppercase">Owner Name</label>
+        <input id="owner_name" type="text" className="p-2 w-full border placeholder-gray-400 rounded-md" placeholder="Enter the Owner Name here" />
+        </div>
+
+        <div className="mb-5">   
+        <label htmlFor="owner_name" className="text-gray-700 uppercase">Email</label>
+        <input id="owner_name" type="email" className="p-2 w-full border placeholder-gray-400 rounded-md" placeholder="Enter the email here" />
+        </div>
+
+        <div className="mb-5">   
+        <label htmlFor="date" className="text-gray-700 uppercase">Date</label>
+        <input id="date" type="date" className="p-2 w-full border placeholder-gray-400 rounded-md" />
+        </div>
+
+        <div className="mb-5">   
+        <label htmlFor="sintoms" className="text-gray-700 uppercase">Date</label>
+        <textarea id="sintoms" 
+        className="p-2 w-full border placeholder-gray-400 rounded-md"
+        />
+        </div>
+
+        <input name="submit" type="submit"
+        className="cursor-pointer transition-all bg-indigo-600 hover:shadow-md hover:bg-indigo-800 w-full p-3 text-white uppercase font-bold"
+        value="Add Pet" />
+        
+
       </form>
     </div>
   )
